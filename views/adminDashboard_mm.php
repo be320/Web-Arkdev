980784a79bdd7dd2cf4e32a003df5aa5097ec130
Youssef <youssef.ossama2@gmail.com>
2019-07-31 23:25:49 +0200

    added nav bar require to all files

--- views/adminDashboard_mm.php
+++ views/adminDashboard_mm.php
@@ -33,75 +33,9 @@ if(isset($_POST['filter'])){
 
 <body>
 <header>
- 
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark indigo">
-        <a href="home_mm.html" class="navbar-brand" style="color: #a2a2a2"><strong>Welcome</strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Admins
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="createAdmin_basma.html">Create</a>
-                        <a class="dropdown-item" href="adminDashboard_mm.php">Dashboard</a>
-                    </div>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Instructors
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="createInstructor_basma.php">Create</a>
-                        <a class="dropdown-item" href="instructorDashboard_mm.html">Dashboard</a>
-                    </div>
-
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Students
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="createStudent_basma.html">Create</a>
-                        <a class="dropdown-item" href="studentDashboard_mm.php">Dashboard</a>
-                    </div>
-
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Courses
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="createCourse_basma.php">Create</a>
-                        <a class="dropdown-item" href="courseDashboard_mm.php">Dashboard</a>
-                    </div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Tracks
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="createTrack_basma.html">Create</a>
-                        <a class="dropdown-item" href="trackDashboard_mm.html">Dashboard</a>
-
-                    </div>
-
-                <li class="nav-item dropdown">
-                    <a role="button" href="teach.php" class="navbar" style="color: #a2a2a2">Teach</a>
-
-
-            </ul>
-			<ul class="nav navbar-nav navbar-right">
-      <li><a href="/app/Controllers/logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-    </ul>
-        </div>
-    </nav>
-	
-</header>
-
+<?php
+require_once(__DIR__.'/../app/Controllers/header.php');
+?>
 <!------------------------------------------------------------------------------------------------------------------->
 <div class="main">
     <div class="main-img">
@@ -124,7 +58,7 @@ if(isset($_POST['filter'])){
                 <th scope="col">ID</th>
                 <th scope="col">Name</th>
                 <th scope="col">Email</th>
-                <th colspan="2">Actions</th>
+                <th scope="col" colspan="2">Actions</th>
             </tr>
 		</thead>
 		<tbody> 
@@ -148,9 +82,9 @@ if(isset($_POST['filter'])){
           echo('<tr>');
           echo('<th>'.$admin->getId() .'</td>');
           echo('<td>'.$admin->getName().'</td>'); 
-          echo('<td style="border-right:2px solid black;">' . $admin->getEmail() . '</td>'); 
-          echo('<td style="text-align:center;"><a href="/views/adminEdit_nada.php?id=' . $admin->getId() . '"><button type="button" class="btn btn-primary">Edit</button></a></td>'); 
-          echo('<td style="text-align:center;"><a href="/../app/Controllers/deleteAdmin.php?id=' . $admin->getId() . '"><button '.makeSure($admin).'type="button" class="btn btn-danger">Delete</button></a></td>');   
+          echo('<td>' . $admin->getEmail() . '</td>'); 
+          echo('<td><a href="/views/adminEdit_nada.php?id=' . $admin->getId() . '"><button type="button" class="btn btn-primary">Edit</button></a></td>'); 
+          echo('<td><a href="/../app/Controllers/deleteAdmin.php?id=' . $admin->getId() . '"><button '.makeSure($admin).'type="button" class="btn btn-danger">Delete</button></a></td>');   
           echo('</tr>');
         }
       }catch(Exception $e){
---
+++ views/createTrack_basma.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+         <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/all.css">
+      
+        <title>New Admin Form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+	<header> 
+    <?php
+    require_once(__DIR__.'/../app/Controllers/header.php');
+    ?>
+    <body>
+        <div class="main">
+		
+		<div class="main-img">
+        <img src="../images/books.jpg" class="banner" alt="banner"/>
+        </div>
+		<br><br><br>
+             <div class="container">
+              <div class="row justify-content-center align-items-center ">
+                   <div class="col-sm-6 align-self-center auth-wrapper">
+                        <div class="auth-intro">
+                            <h1 class="auth-title">Create Track Form</h1>
+            
+              
+                            <form id="NewTrackForm" action="/app/Controllers/createTrack.php" method="post">
+                 <div class="form-group">
+                     <i class="far fa-user"></i>
+                     <label for="name">Name</label>
+                     <input id="Name" name="name" type="text" placeholder="Enter your full name" class="form-control" required>
+                 </div>
+                 
+                 <div class="text-center submit-btn">
+                    <button name="submit" type="Submit" class="btn btn-primary" >Submit</button>
+                 </div>
+                
+                 </form>
+                 </div>
+              </div>
+         </div>
+    </div>
+            
+<script src="js/jquery-3.3.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.validate.js"></script>
+<script src="js/main.js"></script>
+            
+</body>
+</html>
